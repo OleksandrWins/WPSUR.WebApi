7785ae2709b3eafcdc7d058cb1f8be133679d032
Yelyzaveta Drozd <elizavetadrozd266@gmail.com>
2023-02-01 08:28:32 +0000

    Merged PR 8: # 60: Setup database functionality
    I configured entities which are related to create topic feature.
    
    Related work items: #60

--- WPSUR.Repository/Entities/SubTagEntity.cs
+++ WPSUR.Repository/Entities/SubTagEntity.cs
@@ -3,17 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WPSUR.Repository.Entities
 {
-    [Table("Post")]
-    public sealed class PostEntity : ManageableEntityBase
+    [Table("SubTag")]
+    public sealed class SubTagEntity : ManageableEntityBase
     {
         [MaxLength(50)]
         public string Title { get; set; }
 
-        [MaxLength(1000)]
-        public string Body { get; set; }
-
         public MainTagEntity MainTag { get; set; }
 
-        public ICollection<SubTagEntity> SubTags { get; set; }
+        public ICollection<PostEntity> Posts { get; set; }
     }
 }
