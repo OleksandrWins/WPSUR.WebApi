757917a482c6728b0356de374644a37a2dd99cce
Yelyzaveta Drozd <elizavetadrozd266@knu.ua>
2023-02-06 21:24:16 +0200

    Updated.

--- WPSUR.Services/Services/MainTagService.cs
+++ WPSUR.Services/Services/MainTagService.cs
@@ -1,9 +1,6 @@
 ﻿using WPSUR.Repository.Entities;
 using WPSUR.Repository.Interfaces;
-using WPSUR.Repository.Repositories;
 using WPSUR.Services.Interfaces;
-using WPSUR.Services.Models.Post;
-using WPSUR.Services.Models.Tags;
 
 namespace WPSUR.Services.Services
 {
@@ -11,43 +8,17 @@ namespace WPSUR.Services.Services
     {
         private readonly IMainTagRepository _mainTagRepository;
 
-
         public MainTagService(IMainTagRepository mainTagRepository)
         {
-            _mainTagRepository = mainTagRepository ?? throw new ArgumentNullException(nameof(mainTagRepository)); 
+            _mainTagRepository = mainTagRepository ?? throw new ArgumentNullException(nameof(mainTagRepository));
         }
 
-        //public async Task<MainTagEntity> GetOrCreateMainTagAsync(string mainTagTitle)
-        //{
-        //    MainTagEntity mainTag = await _mainTagRepository.GetMainTagByTitleAsync(mainTagTitle);
-        //    if (mainTag != null)
-        //    {
-        //        return mainTag;
-        //    }
-        //    if (string.IsNullOrWhiteSpace(mainTagTitle))
-        //    {
-        //        throw new NullReferenceException("The main tag is empty.");
-        //    }
-        //    mainTagTitle = mainTagTitle.Trim();
-        //    mainTagTitle.ToUpper();
-        //    MainTagEntity mainTagEntity = new MainTagEntity()
-        //    {
-        //        Id = Guid.NewGuid(),
-        //        Title = mainTagTitle,
-        //        CreatedDate = DateTime.UtcNow
-        //    };
-        //    return mainTagEntity;
-        //}
-        public async Task<MainTagModel> GetOrCreateMainTagAsync(string mainTagTitle)
+        public async Task<MainTagEntity> GetOrCreateMainTagAsync(string mainTagTitle)
         {
             MainTagEntity mainTag = await _mainTagRepository.GetMainTagByTitleAsync(mainTagTitle);
             if (mainTag != null)
             {
-                MainTagModel mainTagModelFromDatabase = new MainTagModel()
-                {
-                    Title = mainTagTitle
-                };
-                return mainTagModelFromDatabase;
+                return mainTag;
             }
             if (string.IsNullOrWhiteSpace(mainTagTitle))
             {
@@ -63,6 +34,7 @@ namespace WPSUR.Services.Services
             };
             return mainTagEntity;
         }
+
         public async Task<MainTagEntity> AddPostToMainTagAsync(PostEntity post, MainTagEntity mainTag)
         {
             mainTag.Posts.Add(post);
